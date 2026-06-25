7b14cc3027c98f2cf174f861328a5b42bd23aa50
Petar Sakotic <applesakota@gmail.com>
2026-06-25 12:57:06 +0200

    Initial Commit

---
+++ MyPayments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPayments
+//
+//  Created by Petar Sakotic on 6/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
